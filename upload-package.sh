4741738cc42e62fe8f8e6ff8a66b07f50e885319
David Li <jiawei.davidli@gmail.com>
2025-08-12 16:43:59 +0000

    Update uv lockfile and fix pypi upload script
    Signed-off-by: David Li <jiawei.davidli@gmail.com>

--- upload-package.sh
+++ upload-package.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python3 -m build --sdist .
-twine upload dist/chatterbox-vllm-$(cat .latest-version.generated.txt).tar.gz
+twine upload dist/chatterbox_vllm-$(cat .latest-version.generated.txt).tar.gz
